416ae82c015eb4530bb981af48e7eb9cdd8b062a
Maksim Podlesniy <cryptspirit@gmail.com>
2014-08-12 22:10:12 +0300

    mymainmenu

--- rc.lua
+++ rc.lua
@@ -86,17 +86,13 @@ end
 
 -- {{{ Menu
 -- Create a laucher widget and a main menu
-myawesomemenu = {
-   { "manual", terminal .. " -e man awesome" },
-   { "edit config", editor_cmd .. " " .. awesome.conffile },
-   { "restart", awesome.restart },
-   { "quit", awesome.quit }
+powerawesomemenu = {
+    { "Quit", awesome.quit },
+    { "Poweroff", "sudo shutdown -h now" },
+    { "Reboot", "sudo shutdown -r now" },
+    { "Hibernate", "sudo pm-hibernate" }
 }
-
-mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
-                                    { "open terminal", terminal }
-                                  }
-                        })
+mymainmenu = awful.menu({ items = { { "Power Control", powerawesomemenu } } })
 
 -- Menubar configuration
 menubar.utils.terminal = terminal -- Set the terminal for applications that require it
